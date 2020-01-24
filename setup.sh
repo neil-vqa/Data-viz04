61684242c57806cbb20e72ff7727dd34986f9349
Neil Van <49968200+neil-vqa@users.noreply.github.com>
2020-01-24 19:18:32 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,5 @@
-mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"nvqa.business@gmail.com" \n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit 
+echo “[server]
+headless = true
+port = $PORTenableCORS = false
+” > ~/.streamlit/config.toml
